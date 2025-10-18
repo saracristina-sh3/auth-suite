aa86d9db35988af6c63b245fdd78460ecac22f4d
saracristina-sh3 <sh3.sara.pereira@gmail.com>
2025-10-18 10:37:16 -0300

    [CHORE] - colors

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -11,44 +11,69 @@ import 'primeicons/primeicons.css'
 import ThemeSwitcher from './components/theme/ThemeSwitcher.vue';
 import Aura from '@primeuix/themes/aura';
 
-const MyPreset = definePreset(Aura, {
-semantic: {
-        colorScheme: {
-          light: {
-      surface: {
-        0: "#ffffff",
-        50: "{slate.50}",
-        100: "{slate.100}",
-        200: "{slate.200}",
-        300: "{slate.300}",
-        400: "{slate.400}",
-        500: "{slate.500}",
-        600: "{slate.600}",
-        700: "{slate.700}",
-        800: "{slate.800}",
-        900: "{slate.900}",
-        950: "{slate.950}",
+
+const sh3Colors = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: "var(--selenium-50)",
+      100: "var(--selenium-100)",
+      200: "var(--selenium-200)",
+      300: "var(--selenium-300)",
+      400: "var(--selenium-400)",
+      500: "var(--selenium-500)",
+      600: "var(--selenium-600)",
+      700: "var(--selenium-700)",
+      800: "var(--selenium-800)",
+      900: "var(--selenium-900)",
+      950: "var(--selenium-950)",
+    },
+    secondary: {
+      50: "var(--copper-50)",
+      100: "var(--copper-100)",
+      200: "var(--copper-200)",
+      300: "var(--copper-300)",
+      400: "var(--copper-400)",
+      500: "var(--copper-500)",
+      600: "var(--copper-600)",
+      700: "var(--copper-700)",
+      800: "var(--copper-800)",
+      900: "var(--copper-900)",
+      950: "var(--copper-950)",
+    },
+    colorScheme: {
+      light: {
+        primary: {
+          color: '{selenium.950}',
+          inverseColor: '#ffffff',
+          hoverColor: '{selenium.900}',
+          activeColor: '{selenium.800}'
+        },
+        highlight: {
+          background: '{selenium.950}',
+          focusBackground: '{selenium.700}',
+          color: '#ffffff',
+          focusColor: '#ffffff'
+        }
       },
-      primary: {
-        color: "{primary.500}",
-        contrastColor: "#ffffff",
-        hoverColor: "{primary.600}",
-        activeColor: "{primary.700}",
-      },
-      highlight: {
-        background: "{primary.50}",
-        focusBackground: "{primary.100}",
-        color: "{primary.700}",
-        focusColor: "{primary.800}",
-      },
-      mask: {
-        background: "rgba(0,0,0,0.4)",
-        color: "{surface.200}",
-      },
-          }
+      dark: {
+        primary: {
+          color: '{selenium.50}',
+          inverseColor: '{selenium.950}',
+          hoverColor: '{selenium.100}',
+          activeColor: '{selenium.200}'
+        },
+        highlight: {
+          background: 'rgba(250, 250, 250, .16)',
+          focusBackground: 'rgba(250, 250, 250, .24)',
+          color: 'rgba(255,255,255,.87)',
+          focusColor: 'rgba(255,255,255,.87)'
         }
       }
-        });
+    }
+  }
+})
+const Noir = sh3Colors;
+
 
 const app = createApp(App)
 
@@ -56,12 +81,14 @@ app.use(createPinia())
 app.use(router)
 app.use(PrimeVue, {
   theme: {
-        preset: MyPreset,
+        preset: Noir,
         options: {
             prefix: 'my',
-            darkModeSelector: 'system',
- name: 'primevue',
-        order: 'app-styles, primevue, another-css-library'
+            darkModeSelector: false,
+            cssLayer: {
+            name: 'primevue',
+            order: 'app-styles, primevue, another-css-library'
+            }
     },
           },
   components: {
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import Components from 'unplugin-vue-components/vite';
 import {PrimeVueResolver} from '@primevue/auto-import-resolver';
 
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
