b9b3045d0eec9a6ae10dab8d2b9514acd35460b4
saracristina-sh3 <sh3.sara.pereira@gmail.com>
2025-10-23 17:45:15 -0300

    [CONFIG] - Configuração do docker para produção

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -25,5 +25,14 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
+    // Produção
+//    proxy: {
+//      '/api': {
+//        target: process.env.VITE_API_URL || 'https://192.168.14.230:3000',
+//        changeOrigin: true,
+//        secure: false,
+//        rewrite: (path) => path,
+//      },
+//    },
   }
 })
